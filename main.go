e7cb7d4e88615430be6939632af5add6d6639bae
aniket.godambe <65593787+AniketGodambe@users.noreply.github.com>
2025-03-07 00:34:57 +0530

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 	fmt.Println("Server is getting started...")
 
-	log.Fatal(http.ListenAndServe(":4000", r))
-	log.Println("Server is running on port 4000")
+	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Println("Server is running on port 8080")
 
 }
